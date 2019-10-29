d53f6de2a7ab11292ea69164d8b8b602888bf289
Kishan Trivedi <ktrivedi@v2softinc.com>
2019-10-29 17:53:30 -0400

    Small update

--- My WSDL Code/Server/com/v2soft/training/server/EmployeeForHr_EmployeeForHrPort_Server.java
+++ My WSDL Code/Server/com/v2soft/training/server/EmployeeForHr_EmployeeForHrPort_Server.java
@@ -1,6 +1,7 @@
 
 package com.v2soft.training.server;
 
+import javax.servlet.http.HttpServlet;
 import javax.xml.ws.Endpoint;
 
 /**
@@ -10,21 +11,27 @@ import javax.xml.ws.Endpoint;
  *
  */
 
-public class EmployeeForHr_EmployeeForHrPort_Server{
+public class EmployeeForHr_EmployeeForHrPort_Server extends HttpServlet {
 
-    protected EmployeeForHr_EmployeeForHrPort_Server() throws java.lang.Exception {
+    public EmployeeForHr_EmployeeForHrPort_Server() throws java.lang.Exception {
         System.out.println("Starting Server");
         Object implementor = new EmployeeForHrPortImpl();
-        String address = "http://localhost:8080/EmployeeForHr";
+        String address = "http://localhost:8081/EmployeeForHr";
         Endpoint.publish(address, implementor);
     }
 
-    public static void main(String args[]) throws java.lang.Exception {
+    /*public static void main(String args[]) throws java.lang.Exception {
         new EmployeeForHr_EmployeeForHrPort_Server();
         System.out.println("Server ready...");
 
         Thread.sleep(5 * 60 * 1000);
         System.out.println("Server exiting");
         System.exit(0);
+    }*/
+    
+    public void init() throws javax.servlet.ServletException {
+          System.out.println("----------");
+          System.out.println("---------- Initialized successfully ----------");
+          System.out.println("----------");
     }
 }
